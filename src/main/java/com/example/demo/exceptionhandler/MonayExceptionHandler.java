aa71d1ff1615904e4eee3040b90a18d6649da60d
gustavo-of <gustavo-of@c-3809.agr.go>
2018-06-19 16:17:08 -0300

    1. Validando valores invalidos com bean validation

--- src/main/java/com/example/demo/exceptionhandler/MonayExceptionHandler.java
+++ src/main/java/com/example/demo/exceptionhandler/MonayExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.example.demo.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,29 +11,53 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class MonayExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	@Autowired
 	private MessageSource messageSource;
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
+
 		String menssagemUsuario = messageSource.getMessage("menssagem.invalida", null, LocaleContextHolder.getLocale());
 		String menssagemDesenvolvedor = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Erro(menssagemUsuario, menssagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		List<Erro> erros = Arrays.asList(new Erro(menssagemUsuario, menssagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
-	
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String menssagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String menssagemDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(menssagemUsuario, menssagemDesenvolvedor));
+		}
+
+		return erros;
+	}
+
 	public static class Erro {
 		private String menssagemUsuario;
 		private String menssagemDesenvolvedor;
-		
+
 		public Erro(String menssagemUsuario, String menssagemDesenvolvedor) {
 			this.menssagemUsuario = menssagemUsuario;
 			this.menssagemDesenvolvedor = menssagemDesenvolvedor;
@@ -51,5 +79,5 @@ public class MonayExceptionHandler extends ResponseEntityExceptionHandler {
 			this.menssagemDesenvolvedor = menssagemDesenvolvedor;
 		}
 	}
-	
+
 }
