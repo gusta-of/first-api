a546853da1a0a923ca0713532b17448f5234b54a
gustavo-of <gustavo-of@c-3809.agr.go>
2018-06-19 13:25:09 -0300

    1. att

---
+++ src/main/java/com/example/demo/FirstApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiApplication.class, args);
+	}
+}
