8f583171d64cddfb2cb95f6764d39236d2d436f9
gustavo-of <gustavo-of@c-3809.agr.go>
2018-06-20 15:31:19 -0300

    1. Criado o metodo de deletar uma entidade via Delete, tratando o erro caro o usuario tente deletar uma entidade que não existe mais no banco, retorna uma pagina 404NotFOund retornando as menssagens de recurso não encontrado, tanto para o usuario quanto para o programador

--- src/main/java/com/example/demo/exceptionhandler/MonayExceptionHandler.java
+++ src/main/java/com/example/demo/exceptionhandler/MonayExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -24,6 +27,9 @@ public class MonayExceptionHandler extends ResponseEntityExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 
+	/*
+	 * Trata Erros quando o usuario tenta salvar uma entidade com argumentos invalidos
+	 */
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -40,6 +46,22 @@ public class MonayExceptionHandler extends ResponseEntityExceptionHandler {
 
 		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	/*
+	 * Trata erros de recurso não encontrado,
+	 * Ex..: Usuario apaga uma entidade que não existe na tabela; 
+	 */
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException( EmptyResultDataAccessException ex, WebRequest request) {
+		
+		
+		String menssagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String menssagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(menssagemUsuario, menssagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
 	}
 
 	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
--- src/main/java/com/example/demo/resource/PessoaResource.java
+++ src/main/java/com/example/demo/resource/PessoaResource.java
@@ -4,16 +4,17 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Model.Pessoa;
@@ -45,6 +46,12 @@ public class PessoaResource {
 	@GetMapping("/{codigo}")
 	public Pessoa buscarPeloCodigo(@PathVariable Long codigo) {
 		return pessoarepository.findOne(codigo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoarepository.delete(codigo);
 	}
 
 }
